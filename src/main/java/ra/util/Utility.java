455ec307c4da8b4c4daff3f3c8d108f94a460943
Ray Li <ray00000sina@gmail.com>
2021-07-23 09:54:44 +0800

    update : Refactor deleteFiles(File file).

--- src/main/java/ra/util/Utility.java
+++ src/main/java/ra/util/Utility.java
@@ -13,9 +13,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.text.DecimalFormat;
+import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
@@ -566,21 +566,15 @@ public class Utility {
    * @param file file
    */
   public void deleteFiles(File file) {
-    Consumer<File> deleteFile =
-        (f) -> {
-          file.delete();
-        };
-
-    if (!file.isDirectory()) {
-      deleteFile.accept(file);
-      return;
+    try {
+      Files.walk(Paths.get(file.getPath()))
+          .sorted(Comparator.reverseOrder())
+          .map(Path::toFile)
+          .forEach(File::delete);
+      file.delete();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    for (File f : file.listFiles()) {
-      deleteFiles(f);
-    }
-
-    deleteFile.accept(file);
   }
 
   /**
