92cef4279c7992ce2fbd1dbd030b700360fc6d8d
Ray Li <ray00000sina@gmail.com>
2021-07-23 12:15:38 +0800

    update : Add more debug information for unit test.

--- src/test/java/test/mock/MockNetServiceCommand.java
+++ src/test/java/test/mock/MockNetServiceCommand.java
@@ -12,6 +12,7 @@ public class MockNetServiceCommand extends NetCommandProvider {
 
   @Override
   public void receivedRequest(NetService.NetRequest request) {
-    System.out.println("text[" + request.getText() + "],request[" + request + "]");
+    System.out.println(
+        "class[" + this.getClass() + "],text[" + request.getText() + "],request[" + request + "]");
   }
 }
